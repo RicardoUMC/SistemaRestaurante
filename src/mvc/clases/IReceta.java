f45c308a19fa4ccc5852ca45fbb611ec6d73d695
Ricardo Mora <rurimoca@gmail.com>
2021-06-11 03:33:21 -0500

    Se hicieron las relaciones de Pedido-Cliente y Pedido-Repartidor

--- src/mvc/clases/IReceta.java
+++ src/mvc/clases/IReceta.java
@@ -1,5 +1,5 @@
 package mvc.clases;
 
 public interface IReceta {
-
+    
 }
--- src/mvc/clases/Pedido.java
+++ src/mvc/clases/Pedido.java
@@ -1,5 +1,11 @@
 package mvc.clases;
 
 public class Pedido {
-    
+    protected Repartidor repartidor;
+    protected Cliente cliente;
+
+    public Pedido(Cliente cliente) {
+        this.repartidor = new Repartidor();
+        this.cliente = cliente; 
+    }
 }
