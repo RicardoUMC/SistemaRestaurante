783539b64aa9f0f91a0798fd678ee7b7fa588ce2
Aldozet <aldo2897@gmail.com>
2021-06-11 17:41:16 -0500

    Update Persona.java

--- src/mvc/clases/Persona.java
+++ src/mvc/clases/Persona.java
@@ -1,5 +1,38 @@
 package mvc.clases;
 
 public abstract class Persona {
-    
+
+        private String nombre;
+        private String apellidos;
+        private int edad;
+        private genero;
+
+        void set_nombre(){
+
+        }
+        void get_nombre(){
+
+        }
+
+        void set_apellido(){
+
+        }
+        void get_apellido(){
+
+        }
+
+        void set_edad(){
+
+        }
+        void get_edad(){
+
+        }
+
+        void set_genero(){
+
+        }
+        void get_genero(){
+
+        }
+ 
 }
