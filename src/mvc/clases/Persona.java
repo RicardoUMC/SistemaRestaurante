1ff78be7c93203d2b9a4fdee3796f8ed0eb48403
Aldozet <aldo2897@gmail.com>
2021-06-13 18:52:40 -0500

    .

--- src/mvc/clases/Persona.java
+++ src/mvc/clases/Persona.java
@@ -7,19 +7,19 @@ public abstract class Persona {
     protected int edad;
     protected String genero;
 
-    public abstract void set_nombre();
+    public abstract void set_nombre(String nombre);
 
     public abstract String get_nombre();
 
-    public abstract void set_apellido();
+    public abstract void set_apellido(String apellido);
 
     public abstract String get_apellido();
 
-    public abstract void set_edad();
+    public abstract void set_edad(int edad);
 
     public abstract int get_edad();
 
-    public abstract void set_genero();
+    public abstract void set_genero(String genero);
 
     public abstract String get_genero();
 
--- src/mvc/clases/Repartidor.java
+++ src/mvc/clases/Repartidor.java
@@ -2,8 +2,6 @@ package mvc.clases;
 
 public class Repartidor extends Persona {
 
-    private Persona repaPersona;
-
     protected int repartidor_No;    
     protected boolean disponibilidad;
 
@@ -11,19 +9,71 @@ public class Repartidor extends Persona {
 
 
     }
-
+    @Override
     public void set_nombre(String nombre){
 
         this.nombre=nombre;
     }
-
+    @Override
     public String get_nombre(){
 
-        return repaPersona.nombre;      
+        return nombre;      
+    }
+    @Override
+    public void set_apellido(String apellido) {
+        
+        this.apellidos = apellido;
+        
+    }
+    @Override
+    public String get_apellido() {
+        
+        return apellidos;
+    }
+    @Override
+    public void set_edad(int edad) {
+        
+        this.edad = edad;
+        
+    }
+    @Override
+    public int get_edad() {
+        
+        return edad;
+    }
+    @Override
+    public void set_genero(String genero) {
+        
+        this.genero = genero;
+    }
+    @Override
+    public String get_genero() {
+        
+        return genero;
     }
 
-    
+    public void set_repartidor_No(int repartidor_No){
+
+        this.repartidor_No = repartidor_No;
+
+    }
+
+    public int get_repartidor_No(){
+
+        return repartidor_No;
+
+    }
+
+    public void set_disponibilidad(boolean disponibilidad){
+
+        this.disponibilidad = disponibilidad;
+
+    }
 
 
+    public boolean get_disponibilidad(){
 
+        return disponibilidad;
+
+    }
 }
