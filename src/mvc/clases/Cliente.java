c8a80bb7a6acc04e62a1aa7b700f461fe6095b94
Aldozet <aldo2897@gmail.com>
2021-06-13 20:57:35 -0500

    Update Cliente.java

--- src/mvc/clases/Cliente.java
+++ src/mvc/clases/Cliente.java
@@ -1,5 +1,53 @@
 package mvc.clases;
 
 public class Cliente extends Persona {
+
+    @Override
+    public void set_nombre(String nombre) {
+        
+        
+    }
+
+    @Override
+    public String get_nombre() {
+        
+        return null;
+    }
+
+    @Override
+    public void set_apellido(String apellido) {
+        
+        
+    }
+
+    @Override
+    public String get_apellido() {
+       
+        return null;
+    }
+
+    @Override
+    public void set_edad(int edad) {
+       
+        
+    }
+
+    @Override
+    public int get_edad() {
+        
+        return 0;
+    }
+
+    @Override
+    public void set_genero(String genero) {
+        
+        
+    }
+
+    @Override
+    public String get_genero() {
+       
+        return null;
+    }
     
 }
