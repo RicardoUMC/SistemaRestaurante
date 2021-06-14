1cdc186c5618992069389570a3a5eb21ff6e1f8d
Aldozet <aldo2897@gmail.com>
2021-06-14 15:43:36 -0500

    Update Cliente.java
    Desarrollo de la clase Cliente

--- src/mvc/clases/Cliente.java
+++ src/mvc/clases/Cliente.java
@@ -2,52 +2,81 @@ package mvc.clases;
 
 public class Cliente extends Persona {
 
-    @Override
-    public void set_nombre(String nombre) {
-        
-        
+    protected Persona per;
+    protected String direccion;
+    protected int numero_telefono;
+
+    Cliente() {}
+
+    Cliente(Persona per,String nombre, String apellidos, int edad, String genero) {
+        super(nombre, apellidos, edad, genero);
+       
     }
 
     @Override
-    public String get_nombre() {
-        
-        return null;
-    }
+    public void set_nombre(String nombre){
 
+        this.nombre=nombre;
+    }
+    @Override
+    public String get_nombre(){
+
+        return nombre;      
+    }
     @Override
     public void set_apellido(String apellido) {
         
+        this.apellidos = apellido;
         
     }
-
     @Override
     public String get_apellido() {
-       
-        return null;
+        
+        return apellidos;
     }
-
     @Override
     public void set_edad(int edad) {
-       
+        
+        this.edad = edad;
         
     }
-
     @Override
     public int get_edad() {
         
-        return 0;
+        return edad;
     }
-
     @Override
     public void set_genero(String genero) {
         
-        
+        this.genero = genero;
     }
-
     @Override
     public String get_genero() {
-       
-        return null;
+        
+        return genero;
     }
+
+    public void set_direccion(String direccion){
+
+        this.direccion = direccion;
+
+    }
+
+    public String get_direccion(){
+
+        return direccion;
+    }
+
+    public void set_numero_tel(int telefono){
+
+        this.numero_telefono = telefono;
+
+    }
+
+    public int get_numero_tel(){
+
+        return numero_telefono;
+    }    
+
     
 }
