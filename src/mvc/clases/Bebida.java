b843a4d6fc251e7e2c3ce357d712355483d06c3c
Aldrich Avila <aldrichjasnz@gmail.com>
2021-06-15 01:25:28 -0500

    Update Bebida.java

--- src/mvc/clases/Bebida.java
+++ src/mvc/clases/Bebida.java
@@ -1,5 +1,37 @@
 package mvc.clases;
 
 public class Bebida implements IReceta {
+    private String nombre;
+    private float precio;
+    private String ingredientes;
 
+    Bebida(){    };
+
+    Bebida(String nombre, float precio, String ingredientes){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.ingredientes = ingredientes;
+    };
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setprecio(float precio) {
+        this.precio = precio;
+    }
+
+    public float getprecio() {
+        return precio;
+    }
+    public void setingredientes(String ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public String getingredientes() {
+        return ingredientes;
+    }  
 }
